775b440b498f087ee92de55365643bab6ca0d3a1
xCuriousParanoiax <MuhamedRajaby_work@outlook.com>
2024-01-29 13:50:58 +0200

    added js file

---
+++ assignment.js
@@ -0,0 +1,13 @@
+let numbers = [0, 1, 6, 7, 8, 3, 5, 4, 9];
+
+function answer(array) {
+    let lengthOfArray = array.length;
+    let expectedSum = lengthOfArray * (lengthOfArray + 1) / 2;
+    let actualSum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    let answer = expectedSum - actualSum;
+    return answer;
+}
+
+let my_answer = answer(numbers);
+
+console.log(my_answer);
